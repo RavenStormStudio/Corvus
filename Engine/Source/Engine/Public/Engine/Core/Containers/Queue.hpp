7802d5fcfe23507e765344ec944f3d7d2ad1a8df
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:58:18 +0200

    feat(engine): adds queue types definition

---
+++ Engine/Source/Engine/Public/Engine/Core/Containers/Queue.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <queue>
+
+#include "Vector.hpp"
+
+template <typename T>
+using TDequeue = std::deque<T>;
+
+template <typename T, typename TContainer = TDequeue<T>>
+using TQueue = std::queue<T, TContainer>;
+
+template <typename T, typename TContainer = TVector<T>, typename TComparer = std::less<T>>
+using TPriorityQueue = std::priority_queue<T, TContainer, TComparer>;
