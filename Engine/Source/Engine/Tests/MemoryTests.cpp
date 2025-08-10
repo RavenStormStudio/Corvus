8182acdefc55a192c041e173c1438a39e9f4f5b3
Drischdaan <drischdaan@protonmail.com>
2025-08-10 19:10:26 +0200

    test(engine): adds memory tests

---
+++ Engine/Source/Engine/Tests/MemoryTests.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+#include <catch2/catch_all.hpp>
+
+#include "Engine/Core/CoreTypes.hpp"
+#include "Engine/Core/Memory/Memory.hpp"
+
+TEST_CASE("Should allocate", "Memory")
+{
+    size64 Size = 200;
+
+    void* Pointer = FMemory::Allocate(Size);
+    size64 PointerSize = FMemory::GetPointerSize(Pointer);
+
+    REQUIRE(Pointer != nullptr);
+    REQUIRE(PointerSize == Size);
+    
+    FMemory::Free(Pointer);
+}
+
+TEST_CASE("Memory Benchmarks", "Memory")
+{
+    SECTION("Allocate")
+    {
+        BENCHMARK("Allocate 1KB")
+        {
+            size64 Size = 1024;
+            void* Pointer = FMemory::Allocate(Size);
+            FMemory::Free(Pointer);
+        };
+
+        BENCHMARK("Allocate 16KB")
+        {
+            size64 Size = 16384;
+            void* Pointer = FMemory::Allocate(Size);
+            FMemory::Free(Pointer);
+        };
+
+        BENCHMARK("Allocate 1MB")
+        {
+            size64 Size = 1048576;
+            void* Pointer = FMemory::Allocate(Size);
+            FMemory::Free(Pointer);
+        };
+
+        BENCHMARK("Allocate 10MB")
+        {
+            size64 Size = 10485760;
+            void* Pointer = FMemory::Allocate(Size);
+            FMemory::Free(Pointer);
+        };
+    }
+}
