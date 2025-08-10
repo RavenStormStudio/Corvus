5dc06c8e878fdd564bbde8a0710ae716588711ca
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:14:42 +0200

    feat(engine): adds smart pointer definitions

--- Engine/Source/Engine/Private/Engine/Core/Memory/Memory.cpp
+++ Engine/Source/Engine/Private/Engine/Core/Memory/Memory.cpp
@@ -1,11 +1,11 @@
 ﻿#include "Engine/Core/Memory/Memory.hpp"
 
-void* FMemory::Allocate(size64 Size, uint8 Alignment)
+void* FMemory::Allocate(const size64 Size, const uint8 Alignment)
 {
     return _aligned_malloc(Size, Alignment);
 }
 
-void* FMemory::Reallocate(void* OriginalPointer, size64 Size, uint8 Alignment)
+void* FMemory::Reallocate(void* OriginalPointer, const size64 Size, const uint8 Alignment)
 {
     return _aligned_realloc(OriginalPointer, Size, Alignment);
 }
@@ -15,7 +15,27 @@ void FMemory::Free(void* Pointer, [[maybe_unused]] uint8 Alignment)
     _aligned_free(Pointer);
 }
 
-size64 FMemory::GetPointerSize(void* Pointer, uint8 Alignment)
+size64 FMemory::GetPointerSize(void* Pointer, const uint8 Alignment)
 {
     return _aligned_msize(Pointer, Alignment, 0);
+}
+
+void* operator new(const size64 Size)
+{
+    return FMemory::Allocate(Size);
+}
+
+void* operator new [](const size64 Size)
+{
+    return FMemory::Allocate(Size);
+}
+
+void operator delete(void* Pointer) noexcept
+{
+    FMemory::Free(Pointer);
+}
+
+void operator delete [](void* Pointer) noexcept
+{
+    FMemory::Free(Pointer);
 }
---
+++ Engine/Source/Engine/Public/Engine/Core/Memory/SmartPointers.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <memory>
+
+#include "Memory.hpp"
+
+template <typename T, typename TDeleter = std::default_delete<T>>
+using TUniquePtr = std::unique_ptr<T, TDeleter>;
+
+template <typename T>
+using TSharedPtr = std::shared_ptr<T>;
+
+template <typename T>
+using TWeakPtr = std::weak_ptr<T>;
+
+template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
+TUniquePtr<T> MakeUnique(TArguments&&... Arguments)
+{
+    return TUniquePtr<T>(FMemory::NewObject<T>(std::forward<TArguments>(Arguments)...));
+}
+
+template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
+TSharedPtr<T> MakeShared(TArguments&&... Arguments)
+{
+    return TSharedPtr<T>(FMemory::NewObject<T>(std::forward<TArguments>(Arguments)...));
+}
