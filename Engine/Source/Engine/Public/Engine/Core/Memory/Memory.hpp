4ca84c6c4accd5d3af0a9d717e0f9abe598c27d6
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:14:42 +0200

    feat(engine): adds smart pointer definitions

--- Engine/Source/Engine/Public/Engine/Core/Memory/Memory.hpp
+++ Engine/Source/Engine/Public/Engine/Core/Memory/Memory.hpp
@@ -2,8 +2,8 @@
 
 #include "Engine/Core/CoreTypes.hpp"
 
-#include <type_traits>
 #include <memory>
+#include <type_traits>
 
 class ENGINE_API FMemory
 {
@@ -13,33 +13,44 @@ public:
     static void Free(void* Pointer, uint8 Alignment = 8);
 
     static size64 GetPointerSize(void* Pointer, uint8 Alignment = 8);
-    
-    template<typename T, typename... TArguments>
+
+    template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
     static T* NewObject(TArguments&&... Arguments)
     {
         T* Pointer = static_cast<T*>(Allocate(sizeof(T)));
         return std::construct_at<T, TArguments...>(Pointer, std::forward<TArguments>(Arguments)...);
     }
 
-    template<typename T, typename... TArguments>
-    static T* NewObjectAligned(uint8 Alignment, TArguments&&... Arguments)
+    template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
+    static T* NewObjectAligned(const uint8 Alignment, TArguments&&... Arguments)
     {
         T* Pointer = static_cast<T*>(Allocate(sizeof(T), Alignment));
         return std::construct_at<T, TArguments...>(Pointer, std::forward<TArguments>(Arguments)...);
     }
 
-    template<typename T>
+    template <typename T>
     static void DestroyObject(T* Pointer)
     {
         std::destroy_at<T>(Pointer);
         Free(static_cast<void*>(Pointer));
     }
 
-    template<typename T>
-    static void DestroyObjectAligned(T* Pointer, uint8 Alignment)
+    template <typename T>
+    static void DestroyObjectAligned(T* Pointer, const uint8 Alignment)
     {
         std::destroy_at<T>(Pointer);
         Free(static_cast<void*>(Pointer), Alignment);
     }
+
 private:
 };
+
+// ReSharper disable CppClangTidyReadabilityRedundantDeclaration
+
+void* operator new(size64 Size);
+void* operator new[](size64 Size);
+
+void operator delete(void* Pointer) noexcept;
+void operator delete[](void* Pointer) noexcept;
+
+// ReSharper enable CppClangTidyReadabilityRedundantDeclaration
