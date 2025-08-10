94141d4fde286a7f18962ba7bc05b31ae6a16534
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:58:39 +0200

    feat(engine): adds map types definition

---
+++ Engine/Source/Engine/Public/Engine/Core/Containers/Map.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <map>
+#include <unordered_map>
+
+template <typename TKey, typename TValue, typename TComparer = std::less<TKey>>
+using TMap = std::map<TKey, TValue, TComparer>;
+
+template <typename TKey, typename TValue, typename THasher = std::hash<TKey>>
+using TUnorderedMap = std::unordered_map<TKey, TValue, THasher>;
