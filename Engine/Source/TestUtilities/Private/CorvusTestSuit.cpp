f7a690883cbaa42a3ce0afaa9cfca3d611b8e3cb
Drischdaan <drischdaan@protonmail.com>
2025-08-10 19:55:19 +0200

    test(suite): adds flag to not generate xml report files

--- Engine/Source/TestUtilities/Private/CorvusTestSuit.cpp
+++ Engine/Source/TestUtilities/Private/CorvusTestSuit.cpp
@@ -2,15 +2,27 @@
 
 #include <catch2/catch_session.hpp>
 
-int TestMain(int ArgumentCount, char* Arguments[])
+int TestMain(const int ArgumentCount, char* Arguments[])
 {
+    bool bShouldUseXML = true;
+    for (int Index = 0; Index < ArgumentCount; ++Index)
+    {
+        if (strcmp(Arguments[Index], "--no-xml") == 0)
+        {
+            bShouldUseXML = false;
+        }
+    }
+
     Catch::Session Session;
     Catch::ConfigData Config = Session.configData();
     Config.verbosity = Catch::Verbosity::High;
     Config.reporterSpecifications.push_back(Catch::ReporterSpec("Console", {}, Catch::ColourMode::PlatformDefault, {}));
-    Config.reporterSpecifications.push_back(Catch::ReporterSpec("XML", Catch::Optional<std::string>("TestResults.xml"), Catch::ColourMode::PlatformDefault, {}));
+    if (bShouldUseXML)
+    {
+        Config.reporterSpecifications.push_back(Catch::ReporterSpec("XML", Catch::Optional<std::string>("TestResults.xml"), Catch::ColourMode::PlatformDefault, {}));
+    }
     Session.useConfigData(Config);
-    
-    int Result = Session.run();
+
+    const int Result = Session.run();
     return Result;
 }
