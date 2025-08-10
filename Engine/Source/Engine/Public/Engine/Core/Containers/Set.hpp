f1959dab0519bfd1df35fc4303292e2655760681
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:58:58 +0200

    feat(engine): adds set types definition

---
+++ Engine/Source/Engine/Public/Engine/Core/Containers/Set.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <set>
+#include <unordered_set>
+
+template <typename T, typename TComparer = std::less<T>>
+using TSet = std::set<T, TComparer>;
+
+template <typename T, typename THasher = std::hash<T>, typename TComparer = std::equal_to<T>>
+using TUnorderedSet = std::unordered_set<T, THasher, TComparer>;
