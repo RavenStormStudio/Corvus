e76eb48b5224170ff97cea92a2000bba22e67850
Drischdaan <drischdaan@protonmail.com>
2025-08-10 19:10:11 +0200

    feat(engine): adds memory class

---
+++ Engine/Source/Engine/Private/Engine/Core/Memory/Memory.cpp
@@ -0,0 +1,21 @@
+﻿#include "Engine/Core/Memory/Memory.hpp"
+
+void* FMemory::Allocate(size64 Size, uint8 Alignment)
+{
+    return _aligned_malloc(Size, Alignment);
+}
+
+void* FMemory::Reallocate(void* OriginalPointer, size64 Size, uint8 Alignment)
+{
+    return _aligned_realloc(OriginalPointer, Size, Alignment);
+}
+
+void FMemory::Free(void* Pointer, [[maybe_unused]] uint8 Alignment)
+{
+    _aligned_free(Pointer);
+}
+
+size64 FMemory::GetPointerSize(void* Pointer, uint8 Alignment)
+{
+    return _aligned_msize(Pointer, Alignment, 0);
+}
