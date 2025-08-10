e8182d3ebb41271e120c4db93ee008d5bf42f159
Drischdaan <drischdaan@protonmail.com>
2025-08-10 20:41:37 +0200

    feat(engine): adds string type definitions and utility functions

---
+++ Engine/Source/Engine/Public/Engine/Core/Containers/String.hpp
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include <format>
+#include <string>
+#include <Windows.h>
+
+#include "Engine/Core/CoreTypes.hpp"
+
+using FChar = char;
+using FString = std::string;
+using FStringView = std::string_view;
+
+template <typename... TArguments>
+using TFormatString = std::format_string<TArguments...>;
+
+using FWideChar = wchar_t;
+using FWideString = std::wstring;
+using FWideStringView = std::wstring_view;
+
+template <typename... TArguments>
+using TWideFormatString = std::wformat_string<TArguments...>;
+
+namespace String
+{
+    template <typename... TArguments>
+    constexpr FString Format(TFormatString<TArguments...> Format, TArguments&&... Arguments)
+    {
+        return std::format<TArguments...>(Format, std::forward<TArguments>(Arguments)...);
+    }
+
+    template <typename... TArguments>
+    constexpr FWideString Format(TWideFormatString<TArguments...> Format, TArguments&&... Arguments)
+    {
+        return std::format<TArguments...>(Format, std::forward<TArguments>(Arguments)...);
+    }
+
+    inline FWideString Convert(const FString& String)
+    {
+        if (String.empty())
+        {
+            return {};
+        }
+        const int32 Size = MultiByteToWideChar(CP_UTF8, 0, String.c_str(), static_cast<int32>(String.size()), nullptr, 0);
+        FWideString Result(Size, L'\0');
+        MultiByteToWideChar(CP_UTF8, 0, String.c_str(), static_cast<int32>(String.size()), Result.data(), Size);
+        return Result;
+    }
+
+    inline FString Convert(const FWideString& String)
+    {
+        if (String.empty())
+        {
+            return {};
+        }
+        const int32 Size = WideCharToMultiByte(CP_UTF8, 0, String.c_str(), static_cast<int>(String.size()), nullptr, 0, nullptr, nullptr);
+        FString Result(Size, '\0');
+        WideCharToMultiByte(CP_UTF8, 0, String.c_str(), static_cast<int>(String.size()), Result.data(), Size, nullptr, nullptr);
+        return Result;
+    }
+}
