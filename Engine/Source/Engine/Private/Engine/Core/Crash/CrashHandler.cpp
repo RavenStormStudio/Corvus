c192aa073477193a06eb016335b1ae0c5f759f0e
Drischdaan <drischdaan@protonmail.com>
2025-08-11 20:31:04 +0200

    feat(engine): adds crash handler class

---
+++ Engine/Source/Engine/Private/Engine/Core/Crash/CrashHandler.cpp
@@ -0,0 +1,29 @@
+﻿#include "Engine/Core/Crash/CrashHandler.hpp"
+
+#include <Windows.h>
+
+#include "Engine/Core/Crash/Stackwalker.hpp"
+
+int32 FCrashHandler::ExitCode = EXIT_SUCCESS;
+
+void FCrashHandler::Setup()
+{
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+    SetUnhandledExceptionFilter(Handler);
+}
+
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+LONG FCrashHandler::Handler(EXCEPTION_POINTERS* ExceptionPointers)
+{
+    ExitCode = EXIT_FAILURE;
+
+    const FStackWalker StackWalker;
+    const TVector<FStackFrame> StackFrames = StackWalker.CaptureStack();
+    MessageBoxA(nullptr, FStackWalker::FormatStackTrace(StackFrames).c_str(), TEXT("Corvus"), MB_ICONERROR | MB_OK);
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+int32 FCrashHandler::GetExitCode()
+{
+    return ExitCode;
+}
