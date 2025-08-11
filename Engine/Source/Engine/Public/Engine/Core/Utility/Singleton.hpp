0d0fdfc983952c4a93c217e008d5d63f6518bbc2
Drischdaan <drischdaan@protonmail.com>
2025-08-11 12:11:29 +0200

    feat(engine): adds singleton class

---
+++ Engine/Source/Engine/Public/Engine/Core/Utility/Singleton.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Engine/Core/Memory/SmartPointers.hpp"
+
+template <typename T>
+class TSingleton
+{
+private:
+    TSingleton() = default;
+
+public:
+    [[nodiscard]] static TSharedPtr<T> GetInstance()
+    {
+        if (Instance == nullptr)
+        {
+            Instance = MakeShared<T>();
+        }
+        return Instance;
+    }
+
+    static void DestroyInstance()
+    {
+        Instance.reset();
+    }
+
+private:
+    inline static TSharedPtr<T> Instance = nullptr;
+
+    friend T;
+};
