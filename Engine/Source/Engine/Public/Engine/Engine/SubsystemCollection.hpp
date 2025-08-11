0b3decfd31e79ea5019e3656a88337f8958dc282
Drischdaan <drischdaan@protonmail.com>
2025-08-11 12:12:23 +0200

    feat(engine): adds subsystem collection class

---
+++ Engine/Source/Engine/Public/Engine/Engine/SubsystemCollection.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "Engine/Core/Containers/Map.hpp"
+#include "Engine/Core/Containers/Name.hpp"
+#include "Engine/Core/Memory/Memory.hpp"
+#include "Engine/Core/Utility/TypeName.hpp"
+
+class ISubsystem;
+
+class ENGINE_API FSubsystemCollection
+{
+public:
+    FSubsystemCollection();
+    ~FSubsystemCollection();
+
+    NON_COPY_MOVEABLE(FSubsystemCollection)
+
+public:
+    [[nodiscard]] bool8 IsRegistered(FName Name) const;
+    [[nodiscard]] ISubsystem* GetSubsystem(FName Name) const;
+    bool8 Register(FName Name, ISubsystem* Subsystem);
+    bool8 Unregister(FName Name);
+
+    template <typename T> requires std::is_base_of_v<ISubsystem, T>
+    [[nodiscard]] bool8 IsRegistered() const
+    {
+        return IsRegistered(GetTypeNameWithoutPrefix<T>());
+    }
+
+    template <typename T> requires std::is_base_of_v<ISubsystem, T>
+    [[nodiscard]] T* GetSubsystem() const
+    {
+        return GetSubsystem(GetTypeNameWithoutPrefix<T>());
+    }
+
+    template <typename T, typename... TArguments> requires std::is_base_of_v<ISubsystem, T> && std::is_constructible_v<T, TArguments...>
+    bool8 Register(TArguments&&... Arguments)
+    {
+        return Register(GetTypeNameWithoutPrefix<T>(), FMemory::NewObject<T, TArguments...>(std::forward<TArguments>(Arguments)...));
+    }
+
+    template <typename T> requires std::is_base_of_v<ISubsystem, T>
+    bool8 Unregister()
+    {
+        return Unregister(GetTypeNameWithoutPrefix<T>());
+    }
+
+private:
+    TUnorderedMap<uint64, ISubsystem*> Subsystems;
+};
