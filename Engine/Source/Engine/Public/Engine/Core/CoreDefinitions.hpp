deb9f1dcad26bf1aa6f816d3207d068aa3018103
Drischdaan <drischdaan@protonmail.com>
2025-08-11 07:58:09 +0200

    feat(engine): adds core precompiler definitions

---
+++ Engine/Source/Engine/Public/Engine/Core/CoreDefinitions.hpp
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#define FORWARD(...) __VA_ARGS__
+
+#define INNER_STRINGIFY(X) #X
+#define STRINGIFY(X) INNER_STRINGIFY(X)
+
+#define INNER_STRINGIFY_TEXT(Text) TEXT(Text)
+#define STRINGIFY_TEXT(Text) INNER_STRINGIFY_TEXT(Text)
+
+#define NON_COPYABLE(Class, Prefix) \
+    Prefix Class(const Class&) = delete; \
+    Prefix Class& operator=(const Class&) = delete;
+
+#define NON_MOVEABLE(Class, Prefix) \
+    Prefix Class(Class&&) = delete; \
+    Prefix Class& operator=(Class&&) = delete;
+
+#define NON_COPY_MOVEABLE(Class) \
+    NON_COPYABLE(Class, ) \
+    NON_MOVEABLE(Class, )
+
+#define NON_COPY_MOVEABLE_PREFIX(Class, Prefix) \
+    NON_COPYABLE(Class, Prefix) \
+    NON_MOVEABLE(Class, Prefix)
+
+#define DEFAULT_COPYABLE(Class, Prefix) \
+    Prefix Class(const Class&) noexcept = default; \
+    Prefix Class& operator=(const Class&) = default;
+
+#define DEFAULT_MOVEABLE(Class, Prefix) \
+    Prefix Class(Class&&) noexcept = default; \
+    Prefix Class& operator=(Class&&) = default;
+
+#define DEFAULT_COPY_MOVEABLE(Class) \
+    DEFAULT_COPYABLE(Class,) \
+    DEFAULT_MOVEABLE(Class,)
+
+#define DEFAULT_COPY_MOVEABLE_PREFIX(Class, Prefix) \
+    DEFAULT_COPYABLE(Class, Prefix) \
+    DEFAULT_MOVEABLE(Class, Prefix)
+
+#define INNER_APPEND(X, Y) X##Y
+#define APPEND(X, Y) INNER_APPEND(X, Y)
+
+#define UNIQUE_VARIABLE(Prefix) APPEND(Prefix, __LINE__)
+
+#define BIT(X) (1 << X)
