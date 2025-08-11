0291a14a1453f924a4a52b1a06239bc1d7837da5
Drischdaan <drischdaan@protonmail.com>
2025-08-11 12:12:31 +0200

    feat(engine): adds engine class

---
+++ Engine/Source/Engine/Private/Engine/Engine/Engine.cpp
@@ -0,0 +1,36 @@
+﻿#include "Engine/Engine/Engine.hpp"
+
+void FEngine::Initialize()
+{
+    SubsystemCollection = MakeUnique<FSubsystemCollection>();
+}
+
+void FEngine::Shutdown()
+{
+    SubsystemCollection.reset();
+}
+
+bool8 FEngine::IsSubsystemRegistered(const FName Name) const
+{
+    return SubsystemCollection->IsRegistered(Name);
+}
+
+ISubsystem* FEngine::GetSubsystem(const FName Name) const
+{
+    return SubsystemCollection->GetSubsystem(Name);
+}
+
+bool8 FEngine::RegisterSubsystem(const FName Name, ISubsystem* Subsystem) const
+{
+    return SubsystemCollection->Register(Name, Subsystem);
+}
+
+bool8 FEngine::UnregisterSubsystem(const FName Name) const
+{
+    return SubsystemCollection->Unregister(Name);
+}
+
+TSharedPtr<FEngine> GetEngine()
+{
+    return FEngine::GetInstance();
+}
