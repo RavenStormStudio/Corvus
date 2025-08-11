ea4e83bcdfb55713e04ec62647d0b31433516260
Drischdaan <drischdaan@protonmail.com>
2025-08-11 07:58:37 +0200

    feat(engine): adds name class

---
+++ Engine/Source/Engine/Public/Engine/Core/Containers/Name.hpp
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "Map.hpp"
+#include "String.hpp"
+
+#include "Engine/Core/CoreDefinitions.hpp"
+#include "Engine/Core/CoreTypes.hpp"
+
+class ENGINE_API FNameStringPool
+{
+public:
+    [[nodiscard]] static bool8 IsRegistered(uint64 Hash);
+    static void Register(uint64 Hash, const FString& String);
+
+    [[nodiscard]] static const FString& GetString(uint64 Hash);
+    [[nodiscard]] static FStringView GetStringView(uint64 Hash);
+
+private:
+    constexpr static FString EmptyString;
+
+private:
+    static TUnorderedMap<uint64, FString> StringPool;
+};
+
+class ENGINE_API FName
+{
+public:
+    FName();
+    FName(const FStringView& InStringView);
+    ~FName() = default;
+
+    DEFAULT_COPY_MOVEABLE(FName)
+
+public:
+    [[nodiscard]] bool8 IsValid() const;
+
+    [[nodiscard]] FString GetString() const;
+    [[nodiscard]] FStringView GetStringView() const;
+
+public:
+    [[nodiscard]] uint64 GetHash() const { return Hash; }
+
+public:
+    operator uint64() const;
+    operator const uint64() const;
+
+private:
+    uint64 Hash;
+};
