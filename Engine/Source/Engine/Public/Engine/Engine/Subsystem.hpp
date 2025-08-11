099fe44bf94548401b68e270e66ce961deb1eb72
Drischdaan <drischdaan@protonmail.com>
2025-08-11 12:12:12 +0200

    feat(engine): adds subsystem interface

---
+++ Engine/Source/Engine/Public/Engine/Engine/Subsystem.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Engine/Core/CoreDefinitions.hpp"
+
+class ENGINE_API ISubsystem
+{
+public:
+    ISubsystem() = default;
+    virtual ~ISubsystem() = default;
+
+    NON_COPY_MOVEABLE(ISubsystem)
+
+public:
+    virtual void Initialize() = 0;
+    virtual void Deinitialize() = 0;
+
+private:
+};
