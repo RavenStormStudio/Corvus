af08244a79ff85844d8c8850f4a839384a87420c
Drischdaan <drischdaan@protonmail.com>
2025-08-13 22:27:28 +0200

    feat(engine): adds guid class

---
+++ Engine/Source/Engine/Private/Engine/Core/Utility/Guid.cpp
@@ -0,0 +1,81 @@
+﻿#include "Engine/Core/Utility/Guid.hpp"
+
+#include <combaseapi.h>
+
+int32 ParseHexDigit(const FChar Character)
+{
+    int32 Hex;
+    if (Character >= '0' && Character <= '9')
+    {
+        Hex = Character - '0';
+    }
+    else if (Character >= 'a' && Character <= 'f')
+    {
+        Hex = Character + 10 - 'a';
+    }
+    else if (Character >= 'A' && Character <= 'F')
+    {
+        Hex = Character + 10 - 'A';
+    }
+    else
+    {
+        Hex = 0;
+    }
+    return Hex;
+}
+
+uint32 ParseHexNumber(const FStringView String)
+{
+    uint32 Hex = 0;
+    for (const FChar HexChar : String)
+    {
+        Hex *= 16;
+        Hex += ParseHexDigit(HexChar);
+    }
+    return Hex;
+}
+
+FGuid FGuid::NewGuid()
+{
+    FGuid Guid;
+    CoCreateGuid(reinterpret_cast<GUID*>(&Guid));
+    return Guid;
+}
+
+bool8 FGuid::Parse(const FChar* String, FGuid& OutGuid)
+{
+    return Parse(FStringView(String), OutGuid);
+}
+
+bool8 FGuid::Parse(const FString& String, FGuid& OutGuid)
+{
+    return Parse(FStringView(String), OutGuid);
+}
+
+bool8 FGuid::Parse(const FStringView String, FGuid& OutGuid)
+{
+    if (String.length() != 36 || String[8] != '-' || String[13] != '-' || String[18] != '-' || String[23] != '-')
+    {
+        return false;
+    }
+    OutGuid = FGuid(
+        ParseHexNumber(String.substr(0, 8)),
+        ParseHexNumber(String.substr(9, 4)) << 16 | ParseHexNumber(String.substr(14, 4)),
+        ParseHexNumber(String.substr(19, 4)) << 16 | ParseHexNumber(String.substr(24, 4)),
+        ParseHexNumber(String.substr(28, 8))
+    );
+    return true;
+}
+
+void FGuid::Invalidate()
+{
+    A = 0;
+    B = 0;
+    C = 0;
+    D = 0;
+}
+
+FString FGuid::ToString()
+{
+    return String::Format("{:08x}-{:04x}-{:04x}-{:04x}-{:04x}{:08x}", A, B >> 16, B & 0xFFFF, C >> 16, C & 0xFFFF, D);
+}
---
+++ Engine/Source/Engine/Public/Engine/Core/Utility/Guid.hpp
@@ -0,0 +1,94 @@
+﻿#pragma once
+#include "Engine/Core/CoreTypes.hpp"
+#include "Engine/Core/Containers/String.hpp"
+
+struct ENGINE_API FGuid
+{
+public:
+    constexpr FGuid()
+        : A(0), B(0), C(0), D(0)
+    {
+    }
+
+    constexpr FGuid(const uint32 InA, const uint32 InB, const uint32 InC, const uint32 InD)
+        : A(InA), B(InB), C(InC), D(InD)
+    {
+    }
+
+    FGuid(const FString& InString)
+    {
+        if (!Parse(InString, *this))
+        {
+            Invalidate();
+        }
+    }
+
+public:
+    [[nodiscard]] constexpr bool8 IsValid() const
+    {
+        return (A | B | C | D) != 0;
+    }
+
+    void Invalidate();
+    [[nodiscard]] FString ToString();
+
+public:
+    constexpr friend bool8 operator==(const FGuid& First, const FGuid& Second)
+    {
+        return ((First.A ^ Second.A) | (First.B ^ Second.B) | (First.C ^ Second.C) | (First.D ^ Second.D)) == 0;
+    }
+
+    constexpr friend bool8 operator!=(const FGuid& First, const FGuid& Second)
+    {
+        return !(First == Second);
+    }
+
+    constexpr friend bool8 operator<(const FGuid& First, const FGuid& Second)
+    {
+        if (First.D > Second.D)
+            return First.A < Second.A
+                       ? true
+                       : First.A > Second.A
+                       ? false
+                       : First.B < Second.B
+                       ? true
+                       : First.B > Second.B
+                       ? false
+                       : First.C < Second.C
+                       ? true
+                       : First.C > Second.C
+                       ? false
+                       : First.D < Second.D;
+        return First.A < Second.A
+                   ? true
+                   : First.A > Second.A
+                   ? false
+                   : First.B < Second.B
+                   ? true
+                   : First.B > Second.B
+                   ? false
+                   : First.C < Second.C
+                   ? true
+                   : First.C > Second.C
+                   ? false
+                   : First.D < Second.D;
+    }
+
+    constexpr friend bool8 operator>(const FGuid& First, const FGuid& Second)
+    {
+        return !(First < Second);
+    }
+
+public:
+    [[nodiscard]] static FGuid NewGuid();
+
+    static bool8 Parse(const FChar* String, FGuid& OutGuid);
+    static bool8 Parse(const FString& String, FGuid& OutGuid);
+    static bool8 Parse(FStringView String, FGuid& OutGuid);
+
+private:
+    uint32 A;
+    uint32 B;
+    uint32 C;
+    uint32 D;
+};
