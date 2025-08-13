fff2f8bd622c427077658854e3f7e54c42bc9351
Drischdaan <drischdaan@protonmail.com>
2025-08-13 18:06:19 +0200

    feat(engine): adds logger class

---
+++ Engine/Source/Engine/Private/Engine/Core/Logging/Logger.cpp
@@ -0,0 +1,41 @@
+﻿#include "Engine/Core/Logging/Logger.hpp"
+
+#include <spdlog/async.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+DECLARE_LOG_CHANNEL(Temp, All)
+
+static constexpr const FChar* LoggerPattern = "%^[%T] [%t] [%l]%$ %v";
+
+void FLogger::Setup()
+{
+    spdlog::init_thread_pool(8192, 1);
+    spdlog::set_level(static_cast<spdlog::level::level_enum>(ELogSeverity::All));
+    spdlog::set_pattern(LoggerPattern);
+}
+
+void FLogger::Destroy()
+{
+    spdlog::shutdown();
+}
+
+void FLogger::Log(const FLogChannel& Channel, ELogSeverity Severity, const FString& Message)
+{
+    if (Severity < Channel.Severity)
+    {
+        return;
+    }
+    std::shared_ptr<spdlog::logger> Logger = spdlog::get(Channel.ChannelName.GetString());
+    if (Logger == nullptr)
+    {
+        spdlog::sinks_init_list InitializerList = {
+            std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
+        };
+        Logger = std::make_shared<spdlog::async_logger>(Channel.ChannelName.GetString(), InitializerList, spdlog::thread_pool());
+        Logger->set_pattern(LoggerPattern);
+        Logger->set_level(static_cast<spdlog::level::level_enum>(Channel.Severity));
+        spdlog::register_logger(Logger);
+    }
+    Logger->log(static_cast<spdlog::level::level_enum>(Severity), Message);
+}
---
+++ Engine/Source/Engine/Public/Engine/Core/Logging/Logger.hpp
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "Engine/Core/Containers/Name.hpp"
+#include "Engine/Core/Containers/String.hpp"
+
+#include <type_traits>
+
+enum class ENGINE_API ELogSeverity : uint8
+{
+    Trace = 0,
+    Debug = 1,
+    Info = 2,
+    Warn = 3,
+    Error = 4,
+    Critical = 5,
+    All = 0,
+    Off = 6,
+};
+
+struct ENGINE_API FLogChannel
+{
+    FName ChannelName;
+    ELogSeverity Severity;
+};
+
+class ENGINE_API FLogger
+{
+public:
+    static void Setup();
+    static void Destroy();
+
+    static void Log(const FLogChannel& Channel, ELogSeverity Severity, const FString& Message);
+
+    template <typename... TArguments>
+    static void Log(const FLogChannel& Channel, const ELogSeverity Severity, TFormatString<TArguments...> Format, TArguments&&... Arguments)
+    {
+        Log(Channel, Severity, String::Format<TArguments...>(Format, std::forward<TArguments>(Arguments)...));
+    }
+};
+
+#define DEFINE_LOG_CHANNEL_EXTERN(Name) \
+    extern FLogChannel Log##Name##;
+
+#define DECLARE_LOG_CHANNEL(Name, DefaultSeverity) \
+    FLogChannel Log##Name## = { .ChannelName = #Name, .Severity = ELogSeverity::DefaultSeverity };
+
+#define CVLOG(Channel, Severity, Message, ...) FLogger::Log(Channel, ELogSeverity::Severity, Message, __VA_ARGS__)
+
+ENGINE_API DEFINE_LOG_CHANNEL_EXTERN(Temp)
