2af3d3246f21556c1ae55dc52f5eb6c3be5df0c7
Drischdaan <drischdaan@protonmail.com>
2025-08-17 20:06:24 +0200

    feat(engine): adds critical section class

---
+++ Engine/Source/Engine/Private/Engine/Threading/CriticalSection.cpp
@@ -0,0 +1,33 @@
+﻿#include "Engine/Threading/CriticalSection.hpp"
+
+FCriticalSection::FCriticalSection(const uint32 InSpinCount) noexcept
+{
+    if (InSpinCount > 0)
+    {
+        InitializeCriticalSectionAndSpinCount(&CriticalSection, InSpinCount);
+    }
+    else
+    {
+        InitializeCriticalSection(&CriticalSection);
+    }
+}
+
+FCriticalSection::~FCriticalSection() noexcept
+{
+    DeleteCriticalSection(&CriticalSection);
+}
+
+void FCriticalSection::Lock() noexcept
+{
+    EnterCriticalSection(&CriticalSection);
+}
+
+bool8 FCriticalSection::TryLock() noexcept
+{
+    return TryEnterCriticalSection(&CriticalSection);
+}
+
+void FCriticalSection::Unlock() noexcept
+{
+    LeaveCriticalSection(&CriticalSection);
+}
---
+++ Engine/Source/Engine/Public/Engine/Threading/CriticalSection.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Engine/Core/CoreDefinitions.hpp"
+#include "Engine/Core/CoreTypes.hpp"
+#include "Engine/Core/Crash/Assertion.hpp"
+
+class ENGINE_API FCriticalSection final
+{
+public:
+    explicit FCriticalSection(uint32 InSpinCount = 4000) noexcept;
+    ~FCriticalSection() noexcept;
+
+    NON_COPY_MOVEABLE(FCriticalSection)
+
+public:
+    void Lock() noexcept;
+    [[nodiscard]] bool8 TryLock() noexcept;
+    void Unlock() noexcept;
+
+public:
+    [[nodiscard]] CRITICAL_SECTION GetNativeHandle() const { return CriticalSection; }
+
+private:
+    CRITICAL_SECTION CriticalSection;
+};
