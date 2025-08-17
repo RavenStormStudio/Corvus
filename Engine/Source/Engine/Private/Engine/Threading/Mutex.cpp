fc394c02fb664819b70b5eb72712af938f25cdd4
Drischdaan <drischdaan@protonmail.com>
2025-08-17 20:01:51 +0200

    feat(engine): adds mutex class

---
+++ Engine/Source/Engine/Private/Engine/Threading/Mutex.cpp
@@ -0,0 +1,29 @@
+﻿#include "Engine/Threading/Mutex.hpp"
+
+#include "Engine/Core/Crash/Assertion.hpp"
+
+FMutex::FMutex()
+{
+    MutexHandle = CreateMutex(nullptr, false, nullptr);
+    VERIFY(MutexHandle != nullptr, "Failed to create mutex");
+}
+
+FMutex::~FMutex()
+{
+    CloseHandle(MutexHandle);
+}
+
+void FMutex::Lock() const
+{
+    WaitForSingleObject(MutexHandle, INFINITE);
+}
+
+bool8 FMutex::TryLock() const
+{
+    return WaitForSingleObject(MutexHandle, 0) == WAIT_OBJECT_0;
+}
+
+void FMutex::Unlock() const
+{
+    VERIFY(ReleaseMutex(MutexHandle));
+}
---
+++ Engine/Source/Engine/Public/Engine/Threading/Mutex.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+#include "Engine/Core/CoreDefinitions.hpp"
+#include "Engine/Core/CoreTypes.hpp"
+
+class ENGINE_API FMutex
+{
+public:
+    FMutex();
+    ~FMutex();
+
+    NON_COPY_MOVEABLE(FMutex)
+
+public:
+    void Lock() const;
+    [[nodiscard]] bool8 TryLock() const;
+    void Unlock() const;
+
+public:
+    [[nodiscard]] HANDLE GetNativeHandle() const { return MutexHandle; }
+
+private:
+    HANDLE MutexHandle;
+};
