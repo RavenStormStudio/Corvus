bb4cbfe22d3bfaf003868a2071f7ed8f9e0546c7
Drischdaan <drischdaan@protonmail.com>
2025-08-17 20:02:24 +0200

    feat(engine): adds lockable concepts and scoped lock class

---
+++ Engine/Source/Engine/Public/Engine/Threading/Lockable.hpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "Engine/Core/CoreDefinitions.hpp"
+#include "Engine/Core/CoreTypes.hpp"
+
+#include <type_traits>
+
+template <typename T>
+concept CBaseLockable = requires(T& Lockable)
+{
+    Lockable.Lock();
+    Lockable.Unlock();
+};
+
+template <typename T>
+concept CLockable = CBaseLockable<T> && requires(T& Lockable)
+{
+    { Lockable.TryLock() } -> std::convertible_to<bool8>;
+};
+
+template <CLockable TLockable>
+struct TScopedLock final
+{
+    explicit TScopedLock(TLockable& InLockable)
+        : Lockable(InLockable)
+    {
+        Lockable.Lock();
+    }
+
+    ~TScopedLock()
+    {
+        Lockable.Unlock();
+    }
+
+    NON_COPY_MOVEABLE(TScopedLock)
+
+private:
+    TLockable& Lockable;
+};
