497190ccdb5ba788f1f19f8c01a6283e5c2e355b
Drischdaan <drischdaan@protonmail.com>
2025-08-17 20:21:54 +0200

    feat(engine): adds condition variable class

---
+++ Engine/Source/Engine/Private/Engine/Threading/ConditionVariable.cpp
@@ -0,0 +1,63 @@
+﻿#include "Engine/Threading/ConditionVariable.hpp"
+
+#include "Engine/Threading/CriticalSection.hpp"
+
+FConditionVariable::FConditionVariable()
+{
+    InitializeConditionVariable(&ConditionVariable);
+}
+
+FConditionVariable::~FConditionVariable() = default;
+
+void FConditionVariable::NotifyOne()
+{
+    WakeConditionVariable(&ConditionVariable);
+}
+
+void FConditionVariable::NotifyAll()
+{
+    WakeAllConditionVariable(&ConditionVariable);
+}
+
+void FConditionVariable::Wait(const FCriticalSection& CriticalSection)
+{
+    WaitForInternal(CriticalSection, std::chrono::milliseconds(INFINITE));
+}
+
+void FConditionVariable::Wait(const FCriticalSection& CriticalSection, const FPredicateFunction& Predicate)
+{
+    while (!Predicate())
+    {
+        WaitForInternal(CriticalSection, std::chrono::milliseconds(INFINITE));
+    }
+}
+
+bool8 FConditionVariable::WaitFor(const FCriticalSection& CriticalSection, const std::chrono::milliseconds Timeout)
+{
+    return WaitForInternal(CriticalSection, Timeout);
+}
+
+bool8 FConditionVariable::WaitFor(const FCriticalSection& CriticalSection, const std::chrono::milliseconds Timeout, const FPredicateFunction& Predicate)
+{
+    const auto EndTime = std::chrono::steady_clock::now() + Timeout;
+    while (!Predicate())
+    {
+        const auto Now = std::chrono::steady_clock::now();
+        if (Now >= EndTime)
+        {
+            return false;
+        }
+        const auto RemainingTime = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - Now);
+        if (!WaitForInternal(CriticalSection, RemainingTime))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool8 FConditionVariable::WaitForInternal(const FCriticalSection& CriticalSection, const std::chrono::milliseconds Timeout)
+{
+    CRITICAL_SECTION NativeCriticalSection = CriticalSection.GetNativeHandle();
+    return SleepConditionVariableCS(&ConditionVariable, &NativeCriticalSection, static_cast<DWORD>(Timeout.count()));
+}
---
+++ Engine/Source/Engine/Public/Engine/Threading/ConditionVariable.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <chrono>
+#include <Windows.h>
+
+#include "Engine/Core/CoreDefinitions.hpp"
+#include "Engine/Core/CoreTypes.hpp"
+#include "Engine/Core/Utility/Function.hpp"
+
+class FCriticalSection;
+
+class ENGINE_API FConditionVariable
+{
+public:
+    using FPredicateFunction = TFunction<bool8()>;
+
+public:
+    FConditionVariable();
+    ~FConditionVariable();
+
+    NON_COPY_MOVEABLE(FConditionVariable)
+
+public:
+    void NotifyOne();
+    void NotifyAll();
+
+public:
+    void Wait(const FCriticalSection& CriticalSection);
+    void Wait(const FCriticalSection& CriticalSection, const FPredicateFunction& Predicate);
+    bool8 WaitFor(const FCriticalSection& CriticalSection, std::chrono::milliseconds Timeout);
+    bool8 WaitFor(const FCriticalSection& CriticalSection, std::chrono::milliseconds Timeout, const FPredicateFunction& Predicate);
+
+private:
+    bool8 WaitForInternal(const FCriticalSection& CriticalSection, std::chrono::milliseconds Timeout);
+
+private:
+    CONDITION_VARIABLE ConditionVariable;
+};
