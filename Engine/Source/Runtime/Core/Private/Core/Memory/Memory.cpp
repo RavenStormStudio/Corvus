0b2c1162b2852c9395818240bc098e820d80e320
Drischdaan <drischdaan@protonmail.com>
2025-09-24 22:29:54 +0200

    feat(core): adds memory class

---
+++ Engine/Source/Runtime/Core/Private/Core/Memory/Memory.cpp
@@ -0,0 +1,30 @@
+﻿// RavenStorm Copyright @ 2025-2025
+
+#include "Core/Memory/Memory.hpp"
+
+#include "mimalloc.h"
+
+void* FMemory::Allocate(const size64 Size, const uint8 Alignment)
+{
+    return mi_malloc_aligned(Size, Alignment);
+}
+
+void* FMemory::Reallocate(void* OldPointer, const size64 Size, const uint8 Alignment)
+{
+    return mi_realloc_aligned(OldPointer, Size, Alignment);
+}
+
+void FMemory::Free(void* OldPointer, [[maybe_unused]] const uint8 Alignment)
+{
+    mi_free_aligned(OldPointer, Alignment);
+}
+
+void FMemory::Copy(const void* SourcePointer, void* TargetPointer, const size64 Size)
+{
+    std::memcpy(TargetPointer, SourcePointer, Size);
+}
+
+size64 FMemory::GetAllocationSize(const void* Pointer)
+{
+    return mi_usable_size(Pointer);
+}
---
+++ Engine/Source/Runtime/Core/Public/Core/Memory/Memory.hpp
@@ -0,0 +1,40 @@
+﻿// RavenStorm Copyright @ 2025-2025
+
+#pragma once
+
+#include <memory>
+#include <type_traits>
+
+class CORE_API FMemory
+{
+public:
+    [[nodiscard]] static void* Allocate(size64 Size, uint8 Alignment = 8);
+    [[nodiscard]] static void* Reallocate(void* OldPointer, size64 Size, uint8 Alignment = 8);
+    static void Free(void* OldPointer, uint8 Alignment = 8);
+
+    static void Copy(const void* SourcePointer, void* TargetPointer, size64 Size);
+
+    [[nodiscard]] static size64 GetAllocationSize(const void* Pointer);
+
+public:
+    template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
+    [[nodiscard]] static T* New(TArguments&&... Arguments)
+    {
+        T* Pointer = static_cast<T*>(Allocate(sizeof(T)));
+        return std::construct_at<T>(Pointer, std::forward<TArguments>(Arguments)...);
+    }
+
+    template <typename T, typename... TArguments> requires std::is_constructible_v<T, TArguments...>
+    [[nodiscard]] static T* NewAligned(const uint8 Alignment, TArguments&&... Arguments)
+    {
+        T* Pointer = static_cast<T*>(Allocate(sizeof(T), Alignment));
+        return std::construct_at<T>(Pointer, std::forward<TArguments>(Arguments)...);
+    }
+
+    template <typename T>
+    static void DestroyObject(T* Pointer, const uint8 Alignment = 8)
+    {
+        std::destroy_at<T>(Pointer);
+        Free(static_cast<void*>(Pointer), Alignment);
+    }
+};
