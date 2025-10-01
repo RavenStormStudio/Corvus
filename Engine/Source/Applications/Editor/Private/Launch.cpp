cfefe45207635b2ae1dbbb197094a89af36935ba
Drischdaan <drischdaan@protonmail.com>
2025-10-01 17:52:25 +0200

    chore(editor): moves windows include to preprocessor if check

--- Engine/Source/Applications/Editor/Private/Launch.cpp
+++ Engine/Source/Applications/Editor/Private/Launch.cpp
@@ -1,10 +1,10 @@
 // RavenStorm Copyright @ 2025-2025
 
-#include <Windows.h>
-
 #if defined(CORVUS_MODE_DEBUG) || defined(CORVUS_MODE_DEVELOPMENT)
 int main()
 #else
+#include <Windows.h>
+
 int APIENTRY WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, int ShowCommand)
 #endif
 {
